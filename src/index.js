419933b634aa2e6d348b51984acbfc1a7acbd99e
Ashhh333 <140720316+Ashhh333@users.noreply.github.com>
2024-06-16 00:10:57 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import style from "../public/styles.css";
+ReactDOM.render(<App />, document.getElementById("root"));
